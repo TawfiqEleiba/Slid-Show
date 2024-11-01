e8930d7f9789f624b245f41d9dfc9158b3513d9d
TawfiqEleiba <tawfiqhashim63@gmail.com>
2024-11-01 15:09:39 +0200

    Add files via upload

---
+++ image slid show/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_slid_show
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SlidpictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int imageno = 1;
+        private void Lodingnextimage()
+        {
+            if (imageno == 4)
+            {
+                imageno = 1;
+            }
+            SlidpictureBox1.ImageLocation = string.Format(@"images\{0}.jpg", imageno);
+            imageno++;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Lodingnextimage();
+        }
+    }
+}
